bc88887d9a4e0981532f44023b2f0b788c47eba5
katjam <katja@neontribe.co.uk>
2015-11-20 21:04:15 +0000

    task index shows data!

--- js/index.js
+++ js/index.js
@@ -27,18 +27,11 @@ app.controller('HeaderCtrl', function ($scope) {
 app.controller('LoginCtrl', function ($scope) {
 });
 
-app.controller('TasksCtrl', function ($scope, $resource) {
-	var Tasks = $resource('https://prompto.smileupps.com/tasks');
-	//$scope.tasks = Tasks.get();
-
-	$scope.tasks = [
-		{'name':'Take Medication','category':'Medication', time: '08:00', completed: true },
-		{'name':'Brush Teeth','category':'Hygene', time: '08:15', completed: true},
-		{'name':'Take Medication','category':'Medication', time: '15:00'},
-		{'name':'Eat Dinner','category':'Food', time: '19:00'},
-		{'name':'Take Medication','category':'Medication', time: '21:00'},
-		{'name':'Charge iPad','category':'Devices', time: '22:00'}
-	];
+app.controller('TasksCtrl', function ($scope, $http) {
+	$http.get('https://prompto.smileupps.com/tasks/_design/tasks/_view/all')
+    .success(function (response){
+	    $scope.tasks = response.rows;
+    });
 
 	$scope.taskClick = function (){
 		console.log("clciked.");
