319da6cc42aa978c65dddc4b6a1dd6456ac2c30a
katjam <katja@neontribe.co.uk>
2015-11-20 19:34:20 +0000

    added css and js Set up dashboard and task index tpl

---
+++ js/index.js
@@ -0,0 +1,70 @@
+var app = angular.module('Prompto', ['ngResource', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/dashboard.html',
+			controller: "DashboardCtrl"
+		})
+		.when('/tasks', {
+			templateUrl: 'views/task-index.html',
+			controller: "TasksCtrl"
+		})
+  .otherwise({
+			redirectTo: '/'
+		});
+});
+
+app.controller('HeaderCtrl', function ($scope) {
+	var patientName = 'Harriet';
+	$scope.message = 'Welcome to '+ patientName +'\'s Control Panel';
+});
+
+app.controller('TasksCtrl', function ($scope, $resource) {
+	var Tasks = $resource('https://prompto.smileupps.com/tasks');
+	//$scope.tasks = Tasks.get();
+
+	$scope.tasks = [
+		{'name':'Take Medication','category':'Medication', time: '08:00', completed: true },
+		{'name':'Brush Teeth','category':'Hygene', time: '08:15', completed: true},
+		{'name':'Take Medication','category':'Medication', time: '15:00'},
+		{'name':'Eat Dinner','category':'Food', time: '19:00'},
+		{'name':'Take Medication','category':'Medication', time: '21:00'},
+		{'name':'Charge iPad','category':'Devices', time: '22:00'}
+	];
+
+	$scope.taskClick = function (){
+		console.log("clciked.");
+	};
+});
+
+app.controller('DashboardCtrl', function ($scope, $resource) {
+	var Tasks = $resource('https://prompto.smileupps.com/tasks');
+	//$scope.tasks = Tasks.get();
+
+	$scope.tasks = [
+		{'name':'Take Medication','category':'Medication', time: '08:00', completed: true },
+		{'name':'Brush Teeth','category':'Hygene', time: '08:15', completed: true},
+		{'name':'Take Medication','category':'Medication', time: '15:00'},
+		{'name':'Eat Dinner','category':'Food', time: '19:00'},
+		{'name':'Take Medication','category':'Medication', time: '21:00'},
+		{'name':'Charge iPad','category':'Devices', time: '22:00'}
+	];
+
+	$scope.taskClick = function (){
+		console.log("clicked.");
+	};
+});
+
+var app = {
+	initialize: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+
+	onDeviceReady: function() {
+		console.log('device ready');
+
+	}
+};
+
+app.initialize();
